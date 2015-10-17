fea7b789a9b2302905ca5b24d73b67a584596fbd
marukawa <d.marukawa@caraquri.com>
2015-10-18 07:06:25 +0900

    その他js追加

---
+++ js/topPage.js
@@ -0,0 +1,12 @@
+function dispSearchTextarea(){
+	var searchWord  = document.searchform1.keywords1.value;
+	return searchWord;
+}
+
+function search(){
+	var searchWord = dispSearchTextarea();
+}
+
+function login(){
+	
+}
