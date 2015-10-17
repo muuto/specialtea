d09da55906445e33c5e2c89620cf5f3f9df7df51
yuiza <nukko.address@me.com>
2015-10-18 05:39:26 +0900

    apiサーバ化 [ dbからの切り分け未実装 ]

--- app.js
+++ app.js
@@ -5,53 +5,24 @@ var pg = require('pg');
 var connection = "tcp://postgres:''@127.0.0.1:5432/testdb";
 var client = new pg.Client(connection);
 
+
 io.sockets.on('connection', function(socket){
-	console.log('reqest: ', socket);
-
 /*
-	こんな感じで処理ごとに受けて返す感じ？
+    こんな感じで処理ごとに受けて返す感じ？
 */
-	//リスト入手
-	socket.on('list', function(req){
-		console.log('message: ' + req);
-		/* database */
-		client.connect(function(err){
-		
-			if(err){
-				return console.error('could not connect to postgres', err);
-			}
-			client.query('select * from solutions;', function(err, result){
-				console.log(result);
+    //リスト入手
+    socket.on('list', function(req){
+        console.log('message: ' + req);
+        /* database */
+        client.connect(function(err){
+            if(err){
+                console.error('could not connect to postgres', err);
+            }
+            client.query('select * from solutions;', function(err, result){
+            	//ここでオブジェクトの切り分け（要相談）
+            	socket.emit('push', result);
+            });
+        });
 
-				io.emit('push', row);
-
-
-			});
-
-			// query.on('row', function(row){
-			// 	row.push(row);
-			// });
-			// query.on('end', function(row,err){
-			// 	io.emit('push' row);
-			// });
-		});
-
-	});
-
-	//keyからValue入手の場合こんな感じ
-	// socket.on('key', function(req){
-	// 	console.log('message: ' + req);
-	// 	/* database */
-	// 	client.connect(function(err){
-		
-	// 	if(err){
-	// 		return console.error('could not connect to postgres', err);
-	// 	}
-	// 	//よしなにかえる
-	// 	client.query('select * from mydata;');
-
-	// 	query.on('end', function(row,err){
-	// 		io.emit('push' row);
-	// 	});
-	// });
+    });
 });
