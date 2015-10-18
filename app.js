89bcdc15f83cc4e42c3662d0fc7b34b315763b85
yuiza <nukko.address@me.com>
2015-10-18 10:17:58 +0900

    一応

--- app.js
+++ app.js
@@ -23,6 +23,45 @@ io.sockets.on('connection', function(socket){
             	socket.emit('push', result);
             });
         });
-
     });
+
+    socket.on('contribute', function(req){
+        console.log('message: ' + req);
+        /* 	 {
+        		id: ?,//わかんなぇ
+				title: 'hogehoge',
+				user_id: 1,
+				error_id: '1111',
+				hashtags: {"#hoge", "#fuga", "#bar"}
+				body: content
+        	   }*/
+        /* database */
+        client.connect(function(err){
+            if(err){
+                console.error('could not connect to postgres', err);
+            }
+            client.query('insert into solutions(id, user_id, titile, hashtags, body) value(req.id, 1, req.title, req.hashtags, req.body);', function(err, result){
+            	//ここでオブジェクトの切り分け（要相談）
+            	socket.emit('push', 'ok');
+            });
+        });
+    });
+
+    //socket.on();
 });
+
+
+
+
+
+
+function getData(){
+
+};
+
+
+// obj -> json
+//var JSONfoo = window.JSON.stringify(foo);
+
+//json -> obj
+//var backToJS = JSON.parse(JSONfoo);
